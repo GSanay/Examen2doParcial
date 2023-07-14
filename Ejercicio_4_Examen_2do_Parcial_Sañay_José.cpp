19eec8df0b8d32398a6f7bb1d9192cacd5846182
GSanay <user@DESKTOP-DLLN4GN>
2023-07-14 16:16:25 -0500

    first commit

---
+++ Ejercicio_4_Examen_2do_Parcial_Sañay_José.cpp
@@ -0,0 +1,23 @@
+//EJERCICIO 4 SAÑAY_JOSÉ//
+
+//Hacer un programa que muestre una tabla de multiplicar hasta el 20 de un número//
+//cualquiera por pantalla, el número se pedirá en el programa principal. Usar procedimiento//
+
+#include <iostream>
+
+using namespace std;
+
+void tablaMultiplicar(int num) {
+    for (int i = 1; i <= 20; i++) {
+        cout << num << " x " << i << " = " << num * i << endl;
+    }
+}
+
+int main() {
+    int num;
+    cout << "Ingrese un numero: ";
+    cin >> num;
+    tablaMultiplicar(num);
+    return 0;
+}
+
